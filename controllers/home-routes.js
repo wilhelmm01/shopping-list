acb3d6c322a5341573e474665b2225d58007eeb4
Your Name <matt14w@gmail.com>
2022-02-06 10:35:18 -0500

    login and signup handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,13 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/new-user', (req, res) => {
+  res.render('signup');
+});
+
+
 module.exports = router;
