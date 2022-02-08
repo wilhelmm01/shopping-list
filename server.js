063e7010fd184c9ef603a3b1d178912e3bcba1f0
Your Name <matt14w@gmail.com>
2022-02-07 20:23:45 -0500

    merging cody and Matt

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
 const sess = {
-  secret: '',
+  secret: 'secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -16,10 +18,9 @@ const sess = {
 
 app.use(session(sess));
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+
+
 
 const hbs = exphbs.create({});
 
