c9b893b976aa259c95084aa137fa25246a50b331
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2022-02-09 19:20:39 -0500

    test push

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,13 +35,20 @@ router.get('/:id',  (req, res) => {
 });
 
 router.post('/',  (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -67,18 +74,24 @@ router.post('/login', (req, res) => {
       return;
     }
 
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+    })
+
     res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
 
-router.post('logout',  (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 router.put('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-
-
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+const sess = {
+  secret: 'secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-const hbs = exphbs.create({helpers});
+app.use(session(sess));
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
