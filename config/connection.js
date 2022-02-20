b06a6ea9cbdf9c568d4dc38e7364043f6cb5b2f1
Cody Webb <webbcody36@gmail.com>
2022-02-19 19:14:49 -0500

    clean up and fixed scripts, also added comments

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,20 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+// this section will need to be added when deployed
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+// }
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
